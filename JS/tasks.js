c1d915f1119ad15c1cd0c009be76f0d622b9a354
Wouter Teunisse <wouterteunisse1002@icloud.com>
2023-06-24 03:58:58 +1000

    Created base design for tasks main page

--- JS/tasks.js
+++ JS/tasks.js
@@ -1,5 +1,15 @@
-const Task = {
-name: 'Homework',
-dueDate: '20/06/23',
-
+const tasks = [];
+const addTask = (ev) => {
+    ev.preventDefault();
+    let task = {
+        taskName: getElementById('taskName').value,
+        taskType: getElementById('taskType').value
+    }
+    tasks.push(task);
+    document.querySelector('form').reset();
+    localStorage.setItem('myTasks', JSON.stringify(tasks));
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('confirm').addEventListener('click', addTask);
+});
